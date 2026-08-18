632f1798d497fed49e7c262cb30cf57f3d5c6dd5
Bruno Ramos <brunoramosdsousa0@gmail.com>
2026-08-18 20:25:16 -0300

    chore: first flask build, and routes

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, send_file, request, redirect, url_for, flash
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+app = Flask(__name__)
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+#funcao para fazer o get da primeira pagina do site
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+#funcao para fazer o get da pagina do player
+@app.route("/player")
+def player():
+	return render_template("player.html")
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
